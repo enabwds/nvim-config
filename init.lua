a0c4d0e38ef29aef17b9dd657d428af7734bb008
Yohann Nihalani <nihalaniyohann15@gmail.com>
2024-09-24 21:35:00 +0530

    added markdownpreview.nvim

---
+++ init.lua
@@ -0,0 +1,18 @@
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
+end
+vim.g.mapleader = " "
+vim.opt.rtp:prepend(lazypath)
+vim.opt.background = "dark" -- set this to dark or light
+vim.cmd("colorscheme oxocarbon")
+require("lazy").setup("plugins")
+require("vim-options")
+
